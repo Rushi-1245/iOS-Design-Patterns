3ddecebfac3fd0cbc27ec443f3562b962a8f0db2
Rushi-1245 <rushikeshgaikwad4899@gmail.com>
2026-06-29 22:50:30 +0530

    Initial commit

---
+++ Observer Pattern/CartObserver/CartObserver/Views/CartObserverApp.swift
@@ -0,0 +1,17 @@
+//
+//  CartObserverApp.swift
+//  CartObserver
+//
+//  Created by Siddhatech on 29/06/26.
+//
+
+import SwiftUI
+
+@main
+struct CartObserverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            EmptyView()
+        }
+    }
+}
