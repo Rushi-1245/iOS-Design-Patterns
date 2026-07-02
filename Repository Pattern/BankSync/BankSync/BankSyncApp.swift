38026822748d91aa54f55d8e277206037b1925b8
Rushi-1245 <rushikeshgaikwad4899@gmail.com>
2026-07-02 20:08:36 +0530

    Initial commit

---
+++ Repository Pattern/BankSync/BankSync/BankSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  BankSyncApp.swift
+//  BankSync
+//
+//  Created by Rushi on 02/07/26.
+//
+
+import SwiftUI
+
+@main
+struct BankSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
