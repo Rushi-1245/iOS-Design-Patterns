9bf66a125e2bf43390c97eb000cfa3e7cda0db58
Rushi-1245 <rushikeshgaikwad4899@gmail.com>
2026-07-02 19:17:40 +0530

    Initial Commit

---
+++ Factory Pattern/PaymentFlow/PaymentFlow/PaymentFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaymentFlowApp.swift
+//  PaymentFlow
+//
+//  Created by Rushi on 02/07/26.
+//
+
+import SwiftUI
+
+@main
+struct PaymentFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
